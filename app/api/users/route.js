51e77cff188e4dd6207586fe0102946591a3c3f4
Astro-D-dev <dashercza@gmail.com>
2024-12-30 00:16:50 +0100

    fix: extended errors

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -12,23 +12,15 @@ export async function GET(req) {
       return NextResponse.json({ error: "Field is required" }, { status: 400 });
     }
 
-    const { data: exactMatch, error: emError } = await supabase
+    const { data: exactMatch } = await supabase
       .from("users")
       .select("*")
       .eq(field, search);
 
-      if (emError) {
-        return NextResponse.json({ error }, { status: 400 });
-      }
-
-    const { data: otherMatches, error: omError } = await supabase
+    const { data: otherMatches } = await supabase
       .from("users")
       .select("*")
       .ilike(field, `%${search}%`);
-
-      if (omError) {
-        return NextResponse.json({ error }, { status: 400 });
-      }
 
     let allMatches = [];
 
@@ -43,25 +35,21 @@ export async function GET(req) {
 
     return NextResponse.json(allMatches);
   } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 404 });
+    return NextResponse.json({ error }, { status: 404 });
   }
 }
 
 export async function POST(req) {
   try {
     const { id, username, name, email, location, tags, interactions, bio } = await req.json();
-    const { data, error } = await supabase
+    const { data } = await supabase
       .from('users')
       .insert([{ id, username, name, email, location, tags, interactions, bio }])
       .select();
 
-      if (error) {
-        return NextResponse.json({ error }, { status: 400 });
-      }
-
     return NextResponse.json(data ? data[0] : { message: 'User created successfully' }, { status: 201 });
   } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 406 });
+    return NextResponse.json({ error }, { status: 406 });
   }
 }
 
@@ -74,19 +62,15 @@ export async function PUT(req, params) {
     }
 
     const { name, email, location, tags, interactions, bio } = await req.json();
-    const { data, error } = await supabase
+    const { data } = await supabase
       .from('users')
       .update([{ name, email, location, tags, interactions, bio }])
       .eq('id', id)
       .select();
 
-      if (error) {
-        return NextResponse.json({ error }, { status: 400 });
-      }
-
     return NextResponse.json(data ? data[0] : { message: 'User updated successfully' }, { status: 201 });
   } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 404 });
+    return NextResponse.json({ error }, { status: 404 });
   }
 }
 
@@ -104,12 +88,8 @@ export async function DELETE(req, params) {
       .eq('id', id)
       .delete();
 
-      if (error) {
-        return NextResponse.json({ error }, { status: 400 });
-      }
-
     return NextResponse.json({ message: 'User deleted successfully' }, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 404 });
+    return NextResponse.json({ error }, { status: 404 });
   }
 }
